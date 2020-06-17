e67ea02468bdcfd50b89442382339b404fd3e580
Geoff Bennett <geoff.bennett@gmail.com>
2020-06-17 17:49:52 +1000

    feat: add screen, timer and input parameters
    Adds screen, timer and input parameters to the game object.

--- ConsoleInvaders/game_object.cpp
+++ ConsoleInvaders/game_object.cpp
@@ -1,1 +1,8 @@
 #include "game_object.h"
+
+game_object::game_object(console_screen* screen, game_timer* timer, keyboard* input)
+{
+	p_screen_ = screen;
+	p_timer_ = timer;
+	p_input_ = input;
+}
--- ConsoleInvaders/game_object.h
+++ ConsoleInvaders/game_object.h
@@ -1,21 +1,28 @@
 #pragma once
 
+#include "console_screen.h"
 #include "game_timer.h"
 #include "keyboard.h"
 
 class game_object
 {
+	
 protected:
+	console_screen* p_screen_;
+	game_timer* p_timer_;
+	keyboard* p_input_;
+	
 	float x_ = 0.0f;
 	float y_ = 0.0f;
 	
 public:
+	game_object(console_screen* screen, game_timer* timer, keyboard* input);
 	virtual ~game_object() = default;
 
 	float get_x() const { return x_; }
 	float get_y() const { return y_; }
 	
-	virtual void update(game_timer* timer, keyboard* input) = 0;
-	virtual void draw(wchar_t* buffer) = 0;
+	virtual void update() = 0;
+	virtual void draw() = 0;
 };
 
